e9cb87b91df97066063c1747e664fcdf09fe5927
hiensiga <hiensiga@gmail.com>
2018-11-06 16:23:36 +0700

    create new project

---
+++ iOSArchRESTClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSArchRESTClient
+//
+//  Created by Doan Phuc Hien on 11/6/18.
+//  Copyright © 2018 hiensiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
